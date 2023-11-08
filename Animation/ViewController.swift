63bb5ec783e2c04064009199fb994574235b79fb
Nickolai Grachev <nickbd95@gmail.com>
2023-11-08 12:31:15 +0300

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Nikolai Grachev on 08.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
