613687fd42f9fe63390e01435af8fcf9d75e23bd
Nickolai Grachev <nickbd95@gmail.com>
2023-11-08 21:36:26 +0300

    app done

---
+++ Animation/ValuesData.swift
@@ -0,0 +1,18 @@
+//
+//  ValuesData.swift
+//  Animation
+//
+//  Created by Nikolai Grachev on 08.11.2023.
+//
+
+import Foundation
+
+final class ValuesData {
+    
+    let preset = ["pop", "slideLeft", "slideRight", "slideDown", "slideUp"]
+    let curve = ["easeIn", "easeOut", "easeInOut", "linear", "spring"]
+    let force = [1.0, 1.2, 1.4, 1.6, 1.8]
+    let duration = [1.0, 1.2, 1.4, 1.6, 1.8]
+    let delay = [1.0, 1.2, 1.4, 1.6, 1.8]
+}
+
